cc9da24fa4aff0435f7ef0de92fb1b3f9e6aa2de
jiwen xin <kevinxin@scripps.edu>
2019-11-15 10:50:42 -0800

    add name field and remove duplicate

--- parser.py
+++ parser.py
@@ -26,9 +26,9 @@ def load_data(data_folder):
         next(csv_reader)
         for _item in csv_reader:
             group_by_semmantic_dict[_item[-2]].append(_item[-1])
-            id_type_mapping[_item[-1]] = _item[-2]
+            id_type_mapping[_item[-1]] = {'type': _item[-2], 'name': _item[1]}
     chemical_related = {}
-    parsed_type_list = ['disease_or_phenotypic_feature', 'gene', 'protein']
+    unique_assocs = set()
     with open(edges_path) as f:
         csv_reader = csv.reader(f, delimiter=',')
         next(csv_reader)
@@ -39,25 +39,29 @@ def load_data(data_folder):
                                                   'umls': _item[4][5:],
                                                   'name': id_type_mapping[_item[4]]['name']}
                 pred = _item[0].lower()
-                semantic_type = id_type_mapping[_item[5]]
-                if semantic_type not in parsed_type_list:
-                    if pred not in chemical_related[_item[4]]:
-                        chemical_related[_item[4]][pred] = {}
-                    if semantic_type not in chemical_related[_item[4]][pred]:
-                        chemical_related[_item[4]][pred][semantic_type] = []
-                    chemical_related[_item[4]][pred][semantic_type].append({'pmid': _item[1].split(';'), 'umls': _item[5][5:]})
+                semantic_type = id_type_mapping[_item[5]]['type']
+                if pred not in chemical_related[_item[4]]:
+                    chemical_related[_item[4]][pred] = {}
+                if semantic_type not in chemical_related[_item[4]][pred]:
+                    chemical_related[_item[4]][pred][semantic_type] = []
+                assoc = _item[4] + pred + str(_item[1]) + _item[5]
+                if assoc not in unique_assocs:
+                    unique_assocs.add(assoc)
+                    gene_related[_item[4]][pred][semantic_type].append({'pmid': _item[1].split(';'), 'umls': _item[5][5:]})
             elif _item[5] in group_by_semmantic_dict['chemical_substance']:
                 if _item[5] not in chemical_related:
                     chemical_related[_item[5]] = {'_id': _item[5][5:],
                                                   'umls': _item[5][5:],
                                                   'name': id_type_mapping[_item[5]]['name']}
                 pred = _item[0].lower() + '_reverse'
-                semantic_type = id_type_mapping[_item[4]]
-                if semantic_type not in parsed_type_list:
-                    if pred not in chemical_related[_item[5]]:
-                        chemical_related[_item[5]][pred] = {}
-                    if semantic_type not in chemical_related[_item[5]][pred]:
-                        chemical_related[_item[5]][pred][semantic_type] = []
-                    chemical_related[_item[5]][pred][semantic_type].append({'pmid': _item[1].split(';'), 'umls': _item[4][5:]})
+                semantic_type = id_type_mapping[_item[4]]['type']
+                if pred not in chemical_related[_item[5]]:
+                    chemical_related[_item[5]][pred] = {}
+                if semantic_type not in chemical_related[_item[5]][pred]:
+                    chemical_related[_item[5]][pred][semantic_type] = []
+                assoc = _item[5] + pred + str(_item[1]) + _item[4]
+                if assoc not in unique_assocs:
+                    unique_assocs.add(assoc)
+                    gene_related[_item[5]][pred][semantic_type].append({'pmid': _item[1].split(';'), 'umls': _item[4][5:]})
     for v in chemical_related.values():
         yield v
